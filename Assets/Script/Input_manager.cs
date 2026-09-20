cbad6410d828078b6476d79f03b2cd9903f7c63a
echo121-owo <xuiyuan019@gmail.com>
2026-09-21 03:40:09 +1000

    ADD UNITY FIRST COMMIT

---
+++ Assets/Script/Input_manager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Inputsystem;
+
+
+public class imput : MonoBehaviour
+{
+    private Player_input Player_input;
+    private Player_input.Player_moveActions move;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
