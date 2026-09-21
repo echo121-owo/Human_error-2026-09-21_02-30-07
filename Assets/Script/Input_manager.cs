15993e4e149dcc9b42b932e9f5e04d89ecbdeb8b
echo121-owo <xuiyuan019@gmail.com>
2026-09-21 13:58:48 +1000

    ADD UNITY 2 COMMIT

--- Assets/Script/Input_manager.cs
+++ Assets/Script/Input_manager.cs
@@ -1,20 +1,37 @@
 using UnityEngine;
-using UnityEngine.Inputsystem;
 
-
-public class imput : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
-    private Player_input Player_input;
+    private Player_input player_input;
     private Player_input.Player_moveActions move;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private CharacterController controller;
+
+    public float speed = 5f;
+
+    void Awake()
     {
-        
+        player_input = new Player_input();
+        move = player_input.Player_move;
+
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 input = move.move.ReadValue<Vector2>();
+
+        Vector3 direction = new Vector3(input.x, 0, input.y);
+
+        controller.Move(direction * speed * Time.deltaTime);
+    }
+
+    void OnEnable()
+    {
+        move.Enable();
+    }
+
+    void OnDisable()
+    {
+        move.Disable();
     }
 }
